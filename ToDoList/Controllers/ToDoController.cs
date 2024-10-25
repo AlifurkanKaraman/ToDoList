b4e30170c57be80bb77ddf75779dc00ec180ee78
AlifurkanKaraman <82064494+AlifurkanKaraman@users.noreply.github.com>
2024-10-25 13:58:11 +0300

    Add project files

---
+++ ToDoList/Controllers/ToDoController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ToDoList.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+        private static List<ToDo> toDoList =
+        [
+            new ToDo("Brush teeth", "Brush teeth in the morning"),
+            new ToDo("Read book", "Read for 30 minutes"),
+            new ToDo("Breakfast", "Make yourself a breakfast"),
+            new ToDo("Study programming", "Do 4 pomodoros for it"),
+            new ToDo("Lunch", "Prepare something for it")
+        ];
+        // GET: api/<ToDoController>
+        [HttpGet]
+        public IEnumerable<ToDo> Get()
+        {
+            return toDoList;
+        }
+
+        // GET api/<ToDoController>/5
+        [HttpGet("{id}")]
+        public ActionResult<ToDo> Get(int id)
+        {
+            var currentToDo = toDoList.FirstOrDefault(ToDo => ToDo.Id == id);
+            if (currentToDo != null)
+            {
+                return Ok(currentToDo);
+            }
+
+            return NotFound($"Requested ToDo with id {id} is not on the list");
+        }
+
+        // POST api/<ToDoController>
+        [HttpPost]
+        public IActionResult Post([FromBody] ToDo newToDo)
+        {
+            if (string.IsNullOrWhiteSpace(newToDo.Title))
+            {
+                return BadRequest("Title is required.");
+            }
+
+            toDoList.Add(newToDo);
+            return Ok("ToDo added successfully.");
+
+        }
+
+        // PUT api/<ToDoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ToDoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ToDoList/ToDo.cs
@@ -0,0 +1,18 @@
+﻿namespace ToDoList
+{
+    public class ToDo
+    {
+        public int Id { get; private set; }
+        private static int _counter = 0;
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public bool IsDone { get; set; } = false;
+        public ToDo(string title, string? description = "No description provided")
+        {
+            Id = _counter++;
+            Title = title;
+            Description = description;
+        }
+    }
+}
